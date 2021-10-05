187d4607907782f73a75cea991b46696abcc37e3
Никита Образцов <ymnyi_nik@mail.ru>
2021-10-05 15:24:48 +0300

    Lab_4

--- Lab_4/Lab_4.py
+++ Lab_4/Lab_4.py
@@ -1,1 +1,17 @@
 
+a = int(input('Сторона a:')) 
+b = int(input('Сторона b:'))
+S = a*b
+P = 2*(a + b)
+print("S =", S)
+print("P =", P)
+
+d = int(input('Диаметр d:')) 
+π = 3.14
+L = π*d 
+print("L =", L)
+
+a = int(input('Число a:')) 
+b = int(input('Число b:'))
+sr = ((a+b)/2)
+print("Среднее арифметическое =", sr)
